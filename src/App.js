51c1340b8b0c86339d7cc157eaf92732d6cf31ce
badis-naceur <naceurbadis@gmail.com>
2022-03-09 15:57:13 +0100

    admin side tasks done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
-
+import Login from "./components/dashboard/Login";
 const App = () => {
   const routing = useRoutes(Themeroutes);
 
-  return <div className="dark">{routing}</div>;
+  return (
+    <div className="dark">
+        {routing}
+    </div>
+  );
 };
 
 export default App;
